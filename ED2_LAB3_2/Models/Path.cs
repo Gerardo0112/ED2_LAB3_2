910deae0911fc8def5cceb54300dd8a6c0c01e62
unknown <gerar0112@gmail.com>
2020-02-22 23:32:06 -0600

    Recorrido.

--- ED2_LAB3_2/Models/Path.cs
+++ ED2_LAB3_2/Models/Path.cs
@@ -7,6 +7,42 @@ namespace ED2_LAB3_2.Models
 {
     public class Path
     {
-       
+        public List<Soda> soda = new List<Soda>();
+        public Node node;
+        public string inOrder()
+        {
+            string content = null;
+            var nodes = Paths(node);
+            if (nodes != null)
+            {
+                foreach (var item in nodes)
+                {
+                    var show = "Name: " + item.Name + "\n" + "Flavor: " + item.Flavor + "\n" + "Volume: " + item.Volume + "\n" + "Price: " + item.Price + "\n" + "Producer House: " + item.Producer_House;
+                    content += show;
+                }
+            }
+            else
+            {
+                content = "EMPTY";
+            }
+
+            return content;
+        }
+        public List<Soda> Paths(Node node)
+        {
+            if (node != null)
+            {
+                Paths(node.leftChild);
+                soda.Add(node.leftVal);
+                Paths(node.intermideateChild);
+                if (node.rightVal != null)
+                {
+                    Paths(node.rightChild);
+                    soda.Add(node.rightVal);
+
+                }
+            }
+            return soda;
+        }
     }
 }
